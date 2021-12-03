84782961d77d04628bdeed419bf840fe30741c52
Arjhunt <89695554+Arjhunt@users.noreply.github.com>
2021-12-03 18:46:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+//this is the js code for flag 
+var request = new XMLHttpRequest();
+
+
+request.open('GET', 'https://restcountries.com/v3.1/all', true);
+
+
+request.send();
+
+request.onload = function () {
+    var data = JSON.parse(request.response);
+    for (var i = 0; i < data.length; i++) {
+        console.log(data[i].flag);
+    }
+}
